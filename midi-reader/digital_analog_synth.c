6e114d8241868b57533c623059ccc06530f60545
christopher rowland <christopher.o.rowland@colorado.edu>
2025-06-24 12:29:44 -0600

    program which works at translating midi messages to synth tones

--- midi-reader/digital_analog_synth.c
+++ midi-reader/digital_analog_synth.c
@@ -16,7 +16,7 @@
 #include <signal.h>
 #include <sys/types.h>
 #include "timer_and_array_vars.h"
-#include <stdio.h> // printf()
+#include <stdio.h> // //printf()
 #include <sys/types.h> // open()
 #include <sys/stat.h> // open()
 #include <fcntl.h> // open()
@@ -28,19 +28,19 @@
 #include <linux/i2c.h>
 #include <stdbool.h>
 
-
-int size_of_envelope_arrays=100;
+int update_milliseconds=10;
+int size_of_envelope_arrays=99;
 int i2cbus_fd=0;
 int midi_input_fd=0;
-int vca_addr=0x60;
-int vco_addr=0x61;
+int vca_addr=0x61;
+int vco_addr=0x60;
 pthread_mutex_t i2cbus_lock;
 
 uint16_t target_note_i2cval =0;
 uint16_t current_note_i2cval =0;
 
-uint16_t target_velocity_i2cval = 35000;
-uint16_t current_velocity_i2cval = 35000;
+uint16_t target_velocity_i2cval = 45000;
+uint16_t current_velocity_i2cval = 45000;
 
 uint16_t current_vca_envelope_position=0;
 uint16_t current_vco_envelope_position=0;
@@ -91,7 +91,7 @@ void reset_cached_values(void){
 	velocity_set=false;
 	note_num_set=false;
 	
-	//printf("reset cached values\n");
+	////printf("reset cached values\n");
 }
 
 bool is_command_byte(uint8_t in_byte){
@@ -103,6 +103,8 @@ bool is_command_byte(uint8_t in_byte){
 
 
 void write_vca_value(uint16_t value){
+	if (value > 40000){value = 55000;}
+	printf("writing vca value:%hu\n",value);
 	//calculate the actual bits to be sent before locking and sending them
 	uint8_t input_array[2];
 	input_array[0]=(uint8_t)(value>>12);
@@ -113,15 +115,19 @@ void write_vca_value(uint16_t value){
 	messages[0].flags=0;
 	messages[0].len = sizeof(input_array);
 	messages[0].buf  = input_array;
+	vca_packets.msgs  = messages;
+    vca_packets.nmsgs = 1;
 	
-	if(ioctl(i2cbus_fd, I2C_RDWR, &vca_packets) < 0) {
-			printf("error sending sent\n");
+	int result = ioctl(i2cbus_fd, I2C_RDWR, &vca_packets);
+		if (result != 1){
+			////printf("vca send result was : %d!!!\n",result);
 		}
 }
 
 
 void write_vco_value(uint16_t value){
 	//calculate the actual bits to be sent before locking and sending them
+	//printf("writing vco value: %hu\n",value);
 	uint8_t input_array[2];
 	input_array[0]=(uint8_t)(value>>12);
 	input_array[1]=(uint8_t)(value>>4);
@@ -131,13 +137,16 @@ void write_vco_value(uint16_t value){
 	vco_messages[0].flags = 0;
 	vco_messages[0].len = sizeof(input_array);
 	vco_messages[0].buf  = input_array;
+	vco_packets.msgs  = vco_messages;
+    vco_packets.nmsgs = 1;
 	if(ioctl(i2cbus_fd, I2C_RDWR, &vco_packets) < 0) {
-			printf("error i2c-send\n");
+			//printf("error i2c-send\n");
 		}
 	
 }
 
 uint16_t get_note_i2c_val(uint8_t note_number){
+	//printf("returning %hu i2c val for note number %d\n",note_array[note_number],note_number);
 	return(note_array[note_number]);
 }
 
@@ -146,9 +155,12 @@ uint16_t get_velocity_i2c_val(uint8_t velocity){
 }
 
 void vca_envelope_runner(bool reset){
+	//printf("vca envelope updater: target i2c val = %d, current i2cval = %d\n", target_velocity_i2cval, current_velocity_i2cval);
+	////printf("vca current selected array = %d, 
 	//already locked.
 	if (reset == true){
 		current_vca_envelope_position=0;
+		
 	}
 
 	if (current_vca_envelope_position > size_of_envelope_arrays){
@@ -159,15 +171,21 @@ void vca_envelope_runner(bool reset){
 		}
 	}
 	if (use_decay_array == true){
-		if (current_velocity_i2cval != target_velocity_i2cval + (envelope_arrays[current_selected_vca_array][current_vca_envelope_position])){
-			write_vca_value(target_note_i2cval + (envelope_arrays[current_selected_vca_array][current_vca_envelope_position]));
-			current_velocity_i2cval=target_note_i2cval + (envelope_arrays[current_selected_vca_array][current_vca_envelope_position]);
+		printf("using decay array\n");
+		printf("decay array value at %d is %hu\n", current_vca_envelope_position, envelope_arrays[current_selected_vca_decay_array][current_vca_envelope_position]);
+		if (current_velocity_i2cval != target_velocity_i2cval + (envelope_arrays[current_selected_vca_decay_array][current_vca_envelope_position])){
+			
+			write_vca_value(target_velocity_i2cval + (envelope_arrays[current_selected_vca_decay_array][current_vca_envelope_position]));
+			current_velocity_i2cval=target_velocity_i2cval + (envelope_arrays[current_selected_vca_decay_array][current_vca_envelope_position]);
 		}
 	}
 	else{
-		if (current_velocity_i2cval != target_velocity_i2cval + (envelope_arrays[current_selected_vca_decay_array][current_vca_envelope_position])){
-			write_vca_value(target_note_i2cval + (envelope_arrays[current_selected_vca_decay_array][current_vca_envelope_position]));
-			current_velocity_i2cval=target_note_i2cval + (envelope_arrays[current_selected_vca_decay_array][current_vca_envelope_position]);
+		    //does the current value = the target velocity + the offset?
+		if (current_velocity_i2cval != target_velocity_i2cval + (envelope_arrays[current_selected_vca_array][current_vca_envelope_position])){
+			////printf("current velocity setting %hu current_velocity_i2cval does not match the target %hu + the offset %hu\n", current_velocity_i2cval, target_velocity_i2cval, envelope_arrays[current_selected_vca_decay_array][current_vca_envelope_position]);
+			write_vca_value(target_velocity_i2cval + (envelope_arrays[current_selected_vca_array][current_vca_envelope_position]));
+			////printf("VCA value written is %hu + %hu\n", target_note_i2cval, envelope_arrays[current_selected_vca_array][current_vca_envelope_position]);
+			current_velocity_i2cval=target_velocity_i2cval + (envelope_arrays[current_selected_vca_array][current_vca_envelope_position]);
 		}
 	}
 		
@@ -182,6 +200,7 @@ void vco_envelope_runner(bool reset){
 	if (reset == true){
 		current_vco_envelope_position=0;
 	}
+	//printf("(2) VCO envelope runner is operating under these settings: current_note_i2cval %hu, target_note_i2cval %hu, current_selected_vco_array %hu, current_vco_envelope_position %hu\n",current_note_i2cval,target_note_i2cval,current_selected_vco_array,current_vco_envelope_position);
 	if (current_vco_envelope_position > size_of_envelope_arrays){
 		if (loop_vco == false){
 		return;}
@@ -203,7 +222,7 @@ void vco_envelope_runner(bool reset){
 
 
 void handle_note_on(uint8_t note_number, uint8_t note_velocity,uint8_t channel){
-	printf("handling note on: notenum %d, velocity %d, channel %d\n",note_number,note_velocity,channel);
+	//printf("(1)handling note on: notenum %d, velocity %d, channel %d\n",note_number,note_velocity,channel);
 	//lock everything, envelope runner must complete
 	pthread_mutex_lock(&i2cbus_lock);
 	//set the target note
@@ -219,11 +238,37 @@ void handle_note_on(uint8_t note_number, uint8_t note_velocity,uint8_t channel){
 	
 }
 
+void set_vco_envelope_array(void){
+	current_selected_vco_array = current_selected_vco_array + 1;
+	if (current_selected_vco_array > 14){
+	current_selected_vco_array=0;}
+	printf("current selected vco array is %d\n", current_selected_vco_array);
+	
+}
+
+void set_vca_envelope_array(void){
+	current_selected_vca_array = current_selected_vca_array + 1;
+	if (current_selected_vca_array > 14) {
+		current_selected_vca_array=0;
+	}
+	printf("current selected vca array is %d\n", current_selected_vca_array);
+	
+}
+
 void handle_note_off(uint8_t note_number, uint8_t channel){
-	printf("handling note off: notenum %d, channel %d\n",note_number,channel);
+	//printf("handling note off: notenum %d, channel %d\n",note_number,channel);
+	if (channel == 9){
+		if (note_number == 36){
+			set_vco_envelope_array();
+		}
+		if (note_number == 37){
+			set_vca_envelope_array();
+		}
+	}
 	int noteoff = get_note_i2c_val(note_number);
 	//if it is not the current note number, ignore it
 	if (noteoff != target_note_i2cval){
+		printf("note number for wrong note, not shutting off\n");
 		return;
 	}
 	//set the vca envelope to decay mode
@@ -236,11 +281,13 @@ void handle_note_off(uint8_t note_number, uint8_t channel){
 }
 
 void envelope_updater(void){
+	//printf("envelope updater ran. Current note val=%hu, target note val=%hu\n", current_note_i2cval,target_note_i2cval);
 	pthread_mutex_lock(&i2cbus_lock);
 	vca_envelope_runner(false);
 	vco_envelope_runner(false);
 	pthread_mutex_unlock(&i2cbus_lock);
 }
+
 
 
 int main(){
@@ -261,7 +308,7 @@ int main(){
 	void* timer_thread(void* arg) {
 		struct timespec req, rem;
 		req.tv_sec = 0;
-		req.tv_nsec = 10 * 1000000;
+		req.tv_nsec = update_milliseconds * 1000000;
 		while (1) {
 			if (nanosleep(&req, &rem) == -1) {
 				if (errno == EINTR) { // Interrupted by signal, try again
@@ -304,7 +351,7 @@ int main(){
 					if (high == 0x9){
 						reset_cached_values();
 						curr_command_value = NOTE_ON;
-						////printf("NOTE ON RECEIVED\n");
+						//////printf("NOTE ON RECEIVED\n");
 						continue;
 						}
 					
@@ -318,14 +365,14 @@ int main(){
 					if (curr_command_value == NOTE_ON && note_num_set && (! velocity_set)){
 						velocity = buffer[0];
 						velocity_set = true;
-						//printf("RUNNING NOTE ON: note#: %d, velocity:%d, channel:%d\n",note_num,velocity,channel);
+						//printf("aRUNNING NOTE ON: note#: %d, velocity:%d, channel:%d\n",note_num,velocity,channel);
 						handle_note_on(note_num,velocity,channel);
 						continue;	
 					}
 					if (curr_command_value == NOTE_OFF && (! note_num_set)){ //Note on received, didn't get note num yet
 						note_num = buffer[0];
-						//printf("RUNNING NOTE OFF WITH NOTE # %d, channel %d\n",note_num,channel);
-						if (note_num != 0){
+						printf("bRUNNING NOTE OFF WITH NOTE # %d, channel %d\n",note_num,channel);
+						if (note_num != -1){
 							handle_note_off(note_num, channel);
 						}
 						continue;
@@ -334,13 +381,13 @@ int main(){
 						velocity = buffer[0];
 						velocity_set = true;
 						//printf("RUNNING NOTE OFF: note #%d velocity:%d, channel %d\n",note_num,velocity,channel);
-						if (note_num != 0){
+						if (note_num != -1){
 							handle_note_off(note_num, channel);
 						}
 						continue;	
 					}
 					if (curr_command_value == NOTE_ON && velocity_set){
-						printf("UNHANDLED NOTE ON WITH note number = %d, velocity = %d, channel = %d\n",note_num,velocity,channel);
+						//printf("UNHANDLED NOTE ON WITH note number = %d, velocity = %d, channel = %d\n",note_num,velocity,channel);
 					}
 						
 				}
